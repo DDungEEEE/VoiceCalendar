24b202721ebd145530d88b2dc8561e146152478c
DDungEEEE <thsdnjsdlr12@gmail.com>
2024-12-28 18:13:46 +0900

    update

---
+++ voicecal/src/main/java/com/dongguk/voicecal/VoicecalApplication.java
@@ -0,0 +1,13 @@
+package com.dongguk.voicecal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoicecalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoicecalApplication.class, args);
+	}
+
+}
---
+++ voicecal/src/test/java/com/dongguk/voicecal/VoicecalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dongguk.voicecal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoicecalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
